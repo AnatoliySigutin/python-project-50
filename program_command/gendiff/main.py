c23d87b0b47a30546ec53836d0be86d954ef08fd
AnatoliySigutin <amoral28@mail.ru>
2025-09-19 13:00:33 +0200

    Modified scrtpts

---
+++ program_command/gendiff/main.py
@@ -0,0 +1,18 @@
+import argparse
+
+def gendiff_h():
+    # Создаем объект парсера
+    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    
+    # Позиционные аргументы (обязательные)
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    
+    # Опция помощи (-h, --help) добавляется автоматически
+    
+    # Парсим аргументы
+    args = parser.parse_args()
+    
+    # Тут идёт логика обработки аргументов и собственно сама разница между файлами
+    print(f"Processing comparison between '{args.first_file}' and '{args.second_file}'.")
+
---
+++ program_command/gendiff/scripts/gendiff_h.py
@@ -0,0 +1,9 @@
+from program_command.gendiff.main import gendiff_h
+
+def main():
+	gendiff_h()
+
+
+
+if __name__ == "__main__":
+    main()
