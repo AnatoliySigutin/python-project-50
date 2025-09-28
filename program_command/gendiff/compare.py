c0cf5be9e6a877fd1d035b2a8163ed623a0c65d2
AnatoliySigutin <amoral28@mail.ru>
2025-09-28 17:51:56 +0200

    add compare function

--- program_command/gendiff/compare.py
+++ program_command/gendiff/compare.py
@@ -1,26 +1,37 @@
 import json
 
-# Функция для загрузки JSON
-def load_json(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        print(f"Ошибка загрузки '{file_path}': файл не найден.")
-        exit(1)
-    except json.JSONDecodeError as err:
-        print(f"Ошибка загрузки '{file_path}': неверный формат JSON ({err}).")
-        exit(1)
+def load_json(filename):
+    """Загружает JSON-файл и возвращает словарь."""
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
 
-# Основная логика программы
-def compare_files(file1, file2):
-    # Загружаем файлы
+def generate_diff(file1, file2):
+    """Генерирует отчёт о различиях между двумя JSON-файлами."""
     data1 = load_json(file1)
     data2 = load_json(file2)
 
-    # Простое сравнение ключей и значений
-    for key in set(data1.keys()) | set(data2.keys()):
+    # Определяем все ключи
+    all_keys = set(data1.keys()) | set(data2.keys())
+
+    results = []
+
+    for key in sorted(all_keys):
         val1 = data1.get(key)
         val2 = data2.get(key)
+
         if val1 != val2:
-            print(f"{key}: {val1 or '<отсутствует>'} → {val2 or '<отсутствует>'}")
+            if val1 is None:
+                results.append(f"+ {key}: {val2}")
+            elif val2 is None:
+                results.append(f"- {key}: {val1}")
+            else:
+                results.append(f"- {key}: {val1}\n+ {key}: {val2}")
+        else:
+            # Если ключ общий и значения совпадают, добавляем его в результат
+            results.append(f"  {key}: {val1}")
+
+    output = "{}\n{}\n{}".format("{", "\n".join(results), "}")
+    return output
+
+
+
