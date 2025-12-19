84ae6491d782875ddb7ad87bdc633da9bc462b11
AnatoliySigutin <amoral28@mail.ru>
2025-12-19 20:28:00 +0200

    update tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,76 +7,124 @@ file3 = 'tests/test_data/file3.json'
 file4 = 'tests/test_data/file1.yaml'
 file5 = 'tests/test_data/file2.yaml'
 file6 = 'tests/test_data/file3.yaml'
+file7 = 'tests/test_data/file_1.yaml'
+file8 = 'tests/test_data/file_2.yaml'
+
 
 def test_generate_diff_json():
     expected = '''{
-- follow: False
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: True
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
 }'''
     result = generate_diff(file1, file2)
-    # Сравниваем построчно
-    assert result.splitlines() == expected.splitlines()
+    assert result == expected
 
 
 def test_generate_diff_with_file3():
-    # Тест сравнения file1 и file3
     expected = '''{
-- follow: False
-  host: hexlet.io
-- proxy: 123.234.53.22
-  timeout: 50
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+    timeout: 50
 }'''
     result = generate_diff(file1, file3)
-    assert result.splitlines() == expected.splitlines()
+    assert result == expected
 
 
 def test_generate_diff_identical_files():
-    # Тест сравнения одного и того же файла
     expected = '''{
-  follow: False
-  host: hexlet.io
-  proxy: 123.234.53.22
-  timeout: 50
+    follow: false
+    host: hexlet.io
+    proxy: 123.234.53.22
+    timeout: 50
 }'''
     result = generate_diff(file1, file1)
-    assert result.splitlines() == expected.splitlines()
+    assert result == expected
 
 
 def test_generate_diff_yaml():
     expected = '''{
-- follow: False
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: True
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
 }'''
     result = generate_diff(file4, file5)
-
-    assert result.splitlines() == expected.splitlines()
+    assert result == expected
 
 
 def test_generate_diff_with_file3_yaml():
     expected = '''{
-- follow: False
-  host: hexlet.io
-- proxy: 123.234.53.22
-  timeout: 50
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+    timeout: 50
 }'''
     result = generate_diff(file4, file6)
-    assert result.splitlines() == expected.splitlines()
+    assert result == expected
 
 
 def test_generate_diff_identical_files_yaml():
     expected = '''{
-  follow: False
-  host: hexlet.io
-  proxy: 123.234.53.22
-  timeout: 50
+    follow: false
+    host: hexlet.io
+    proxy: 123.234.53.22
+    timeout: 50
 }'''
     result = generate_diff(file4, file4)
-    assert result.splitlines() == expected.splitlines()
+    assert result == expected
+
+
+def test_generate_diff_different_file_1_and_file_3_yaml():
+    expected = '''{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}'''
+    result = generate_diff(file7, file8)
+    assert result == expected
