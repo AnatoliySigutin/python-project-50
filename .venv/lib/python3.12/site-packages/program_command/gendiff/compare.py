887b5190213e1e18c8b780ad9e8560834afd5902
AnatoliySigutin <amoral28@mail.ru>
2025-12-26 18:49:29 +0200

    add formatter json

--- .venv/lib/python3.12/site-packages/program_command/gendiff/compare.py
+++ .venv/lib/python3.12/site-packages/program_command/gendiff/compare.py
@@ -3,11 +3,13 @@ from program_command.gendiff.parser import load_json_or_yaml
 from program_command.gendiff.generate_builder import build_diff
 from program_command.gendiff.formatters.stylish import format_stylish
 from program_command.gendiff.formatters.plain import format_plain
+from program_command.gendiff.formatters.json import json_format
 
 
 
 
 def generate_diff(file_left: str, file_right: str, format_name = "stylish"):
+    
     data1 = load_json_or_yaml(file_left)
     data2 = load_json_or_yaml(file_right)
     diff_tree = build_diff(data1, data2)
@@ -16,6 +18,7 @@ def generate_diff(file_left: str, file_right: str, format_name = "stylish"):
         return format_stylish(diff_tree)
     elif format_name == 'plain':
         return format_plain(diff_tree)
-    else:
-        return format_stylish(diff_tree)
+    elif format_name == 'json':
+        return json_format(diff_tree)
+    
     
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 asciinema: https://asciinema.org/a/v6GKoI3Qytw34ZTfx8mUtGIfD
 Recording compare yaml: https://asciinema.org/a/IvHrVjg7YhVC8m2VmhCKaYjp2
 Recordind work of format plain: https://asciinema.org/a/Q89of0WgdIMdVSSEeEXUba4dh
+Recording work formatter json: https://asciinema.org/a/GvVFFknSG0nPGcMv0TM1Cb49f
